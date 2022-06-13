82eb1454252fcf47e2e1ec37b709aa2b9d2619c1
dantruonghtno1 <dantruonghtno1@gmail.com>
2022-06-13 14:06:40 +0700

    update data loaders

--- dataloaders/data_loader.py
+++ dataloaders/data_loader.py
@@ -25,9 +25,11 @@ class data_set(Dataset):
             ind
         )
 
-def get_data_loader(config, data, shuffle = False, drop_last = False, batch_size = None):
-
-    dataset = data_set(data, config)
+def get_data_loader(config, data, shuffle = False, drop_last = False, batch_size = None, use_protoAug = False):
+    if use_protoAug == False:
+        dataset = data_set(data, config)
+    else:
+        dataset = data_set_protoAug(data, config)
 
     if batch_size == None:
         batch_size = min(config.batch_size, len(data))
@@ -44,3 +46,30 @@ def get_data_loader(config, data, shuffle = False, drop_last = False, batch_size
         drop_last=drop_last)
 
     return data_loader
+class data_set_protoAug(Dataset):
+
+    def __init__(self, data,config=None):
+        self.data = data
+        self.config = config
+        self.bert = True
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return (self.data[idx], idx)
+
+    def collate_fn(self, data):
+        """
+        data: list[dict{relation:int, tokens:list}]
+        """
+
+        label = torch.tensor([item[0]['relation'] for item in data])
+        tokens = [torch.tensor(item[0]['tokens']) for item in data]
+        ind = torch.tensor([item[1] for item in data])
+
+        return (
+            label,
+            tokens,
+            ind
+        )
